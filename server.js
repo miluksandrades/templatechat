5c61fad7b9037a4a621b2950f6ee8ced880614fe
miluksandrades <luksandrades18@gmail.com>
2019-02-18 18:07:02 -0300

    update

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/templatechat'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/templatechat/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
