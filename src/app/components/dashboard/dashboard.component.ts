e1b0521e654a1fb8789a6fb129ad4cb0dd5fa4e9
miluksandrades <luksandrades18@gmail.com>
2019-02-18 18:25:45 -0300

    update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,18 +11,51 @@ export class DashboardComponent implements OnInit {
 
   visible: Boolean = true;
 
-  users: Array<any> = [];
+  //users: Array<any> = [];
   idSelect: any;
 
+  "users": [
+    {
+      "uid": 4870,
+      "name": "Pedro Eduardo",
+      "photo": "https://www.shareicon.net/data/128x128/2016/09/01/822761_user_512x512.png"
+
+    },
+    {
+      "uid": 1234,
+      "name": "Clodovil da Silva",
+      "photo": "https://i1.wp.com/www.progressivemedicalcenter.com/wp-content/uploads/2019/01/Female-Doctor-Avatar.png"
+    },
+    {
+      "uid": 3455,
+      "name": "Eduardo Cezar",
+      "photo": "https://pngimage.net/wp-content/uploads/2018/06/gambar-user-png-2.png"
+    }
+  ];
+  "chat": [
+    {
+      "id": 1234,
+      "mensage": "Fala Silva beleza!!!"
+    },
+    {
+      "id": 1234,
+      "mensage": "Bora pro Villa Mix?"
+    },
+    {
+      "id": 4870,
+      "mensage": "Ei já fez o módulo? Tô esperando"
+    },
+    {
+      "id": 3455,
+      "mensage": "Depois do expediente bora no mandela?"
+    }
+  ]
+
   constructor(private api: ConsumeService, private render: Renderer) {
-    this.api.getUsers().subscribe(
-      users => {
-        this.users = users
-      }
-    )
+
   }
 
-  getIdUser(uid:any){
+  getIdUser(uid: any) {
     this.idSelect = uid;
     this.visible = !this.visible;
   }
